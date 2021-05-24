abb936dbd32180c3c1795f187a9738b68621d8e5
thanhhhuy264 <huytra264@gmail.com>
2021-05-24 14:35:20 +0700

    a

--- middlewares/authencation.js
+++ middlewares/authencation.js
@@ -5,12 +5,13 @@ module.exports.checkAuthencation = function(req, res, next){
         let { token } = req.session;
         if(token){
             const check = jwt.verify(token, process.env.KEY)
+            console.log(check)
             if(check){
                 next()
             }
         }
-  
     }catch(err){
+        console.log('Token verify failed')
         return res.redirect('/')
     }
 }
