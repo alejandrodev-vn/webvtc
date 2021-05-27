2a4b1cf76ba9bb047d5aad14a258f04254e66a98
thanhnam2001 <thanhnam180201@gmail.com>
2021-05-27 10:46:54 +0700

    validate form ctsdoanhnghiep

--- routes/ctscanhan.route.js
+++ routes/ctscanhan.route.js
@@ -7,7 +7,6 @@ const controllerCTSCaNhan = require('../controllers/ctscanhan.controller')
 
 
 router.get('/digital-certificate/personal', controllerCTSCaNhan.personal);
-
 router.post('/digital-certificate/personal/add', validateCTSCaNhan(), controllerCTSCaNhan.add);
 router.put('/digital-certificate/personal/edit/:id', validateCTSCaNhan(), controllerCTSCaNhan.update);
 router.delete('/digital-certificate/personal/delete/:id', controllerCTSCaNhan.delete);
