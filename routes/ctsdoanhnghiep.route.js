1466a224c6904373b416373e146305a88240c375
thanhhhuy264 <huytra264@gmail.com>
2021-06-14 13:54:28 +0700

    upload file

--- routes/ctsdoanhnghiep.route.js
+++ routes/ctsdoanhnghiep.route.js
@@ -5,12 +5,12 @@ const router = express.Router();
 const { validateCTSDoanhNghiep } = require('../middlewares/ctsdoanhnghiep.validate')
 const controllerCTSDoanhNghiep = require('../controllers/ctsdoanhnghiep.controller')
 const middlewares = require('../middlewares/authencation')
-var upload = multer({ dest: './public/uploads/org' })
+var upload = require('../multer')
 
 
 router.get('/digital-certificate/organization',middlewares.checkAuthencation, controllerCTSDoanhNghiep.organization);
-router.post('/digital-certificate/organization/add',upload.single('hoSoDN'), validateCTSDoanhNghiep(), controllerCTSDoanhNghiep.add);
-router.post('/digital-certificate/organization/edit',middlewares.checkAuthencation,controllerCTSDoanhNghiep.update);
+router.post('/digital-certificate/organization/add',middlewares.checkAuthencation,upload.single('fileHoSo'), validateCTSDoanhNghiep(), controllerCTSDoanhNghiep.add);
+router.post('/digital-certificate/organization/edit',middlewares.checkAuthencation,upload.single('fileHoSo'),validateCTSDoanhNghiep(),controllerCTSDoanhNghiep.update);
 router.post('/digital-certificate/organization/send-mailOrg/:id',middlewares.checkAuthencation, controllerCTSDoanhNghiep.sendMail);
 router.post('/digital-certificate/organization/send-response', controllerCTSDoanhNghiep.sendResponse);
 router.post('/digital-certificate/organization/handle-form-actions', controllerCTSDoanhNghiep.handleFormActions);
